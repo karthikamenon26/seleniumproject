feeb86565dab5468179c758e8e3bb029f7719fe7
HP <HP@DESKTOP-G8I6835>
2024-05-24 20:53:57 +0530

    commit

--- com.sevenmartsupermarket/src/test/java/com/sevenmartsupermarket/base/LoginTest.java
+++ com.sevenmartsupermarket/src/test/java/com/sevenmartsupermarket/base/LoginTest.java
@@ -10,14 +10,16 @@ public class LoginTest extends Base{
 
 	LoginPage loginPage;
 	HomePage homepage;
-	@Test(groups="sanity")
+	
+	//(groups="sanity")
+	@Test
 	public void verify_Login()
 	{
 		loginPage=new LoginPage(driver);
 		loginPage.login();
 	}
-	
-	@Test(groups="smoke", retryAnalyzer = com.sevenmartsupermarket.listeners.RetryAnalyzer.class)
+	//(groups="smoke", retryAnalyzer = com.sevenmartsupermarket.listeners.RetryAnalyzer.class)
+	@Test
 	public void verifyValidUserLogin() {
 		
 		loginPage = new LoginPage(driver);		
@@ -28,8 +30,8 @@ public class LoginTest extends Base{
 		Assert.assertEquals(actualProfileName, expectedProfileName);	
 		
 	}
-	
-	@Test(groups={"sanity","smoke"})
+	//(groups={"sanity","smoke"})
+	@Test
 	public void verifyInvalidUserLogin() {
 		loginPage=new LoginPage(driver);
 			loginPage.login("hhh","kkkk");
